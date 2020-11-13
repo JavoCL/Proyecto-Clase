8290ebc053c2cd5e458e1085bcd929ced2e2bc3a
JavoCL <javierconchaa@gmail.com>
2020-11-13 11:07:16 -0300

    Clase 13/11
    Control de animaciones por script

---
+++ Assets/ControladorEjemploAnimacion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorEjemploAnimacion : MonoBehaviour
+{
+    public Animator animatorCubo;
+    public bool isCrouched = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            animatorCubo.SetTrigger("jumpTrigger");
+
+        if (Input.GetAxis("Vertical") != 0f)
+            animatorCubo.SetBool("walkBool", true);
+        else
+            animatorCubo.SetBool("walkBool", false);
+
+        if(Input.GetButtonDown("Crouch"))
+        {
+            if(isCrouched == false)
+            {
+                animatorCubo.SetTrigger("crouchTrigger");
+            }
+            else if (isCrouched == true)
+            {
+                animatorCubo.SetTrigger("standingTrigger");
+            }
+            isCrouched = !isCrouched;
+        }
+    }
+}
