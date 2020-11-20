04a59b05c5bfc2853ac1443c72ed7f2929a59641
JavoCL <javierconchaa@gmail.com>
2020-11-20 12:37:49 -0300

    Clase 20/11

---
+++ Assets/ControladorAnimacion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorAnimacion : MonoBehaviour
+{
+    public Animator animatorPrueba;
+    public float velocidadAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("VELOCIDAD ACTUAL: " + animatorPrueba.GetCurrentAnimatorStateInfo(0).speed);
+    }
+}
---
+++ Assets/controladorBlendTree.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorBlendTree : MonoBehaviour
+{
+    public Animator animatorBlendTree;
+
+    [Range(-1f, 1f)]
+    public float valorHorizontal;
+    [Range(-1f, 1f)]
+    public float valorVertical;
+    [Range(0f, 1f)]
+    public float inputJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        valorHorizontal = Input.GetAxis("Horizontal");
+        valorVertical = Input.GetAxis("Vertical");
+        inputJump = Input.GetAxis("Vertical");
+
+        //animatorBlendTree.SetFloat("horizontalFloat", valorHorizontal);
+        //animatorBlendTree.SetFloat("verticalFloat", valorVertical);
+
+        animatorBlendTree.SetFloat("velocidadSalto", inputJump);
+    }
+}
